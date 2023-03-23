25e5a434aa176e4f39ce4783959c0965c8263c9a
Demba BA <badembajob@gmail.com>
2023-03-23 22:41:22 +0100

    Ajout Projet

---
+++ characters.py
@@ -0,0 +1,15 @@
+class Proie:
+    def __init__(self, age, graph , x, y):
+        self.age = age
+        self.graph = graph
+        self.cadre_x = x
+        self.cadre_y = y
+
+class Predateur:
+    def __init__(self, energie, graph, x, y):
+        self.energie = energie
+        self.graph = graph
+        self.cadre_x = x
+        self.cadre_y = y
+# Ne pas chasser 2 fois les même proies
+
---
+++ main.py
@@ -0,0 +1,194 @@
+import itertools
+import tkiteasy as tki
+from characters import Proie, Predateur
+import random
+
+g = tki.ouvrirFenetre(500, 500)
+proies = []
+predateurs = []
+liste = [None].copy() * 20
+cadre = [liste.copy() for _ in range(20)]
+EPRE = 4
+APROIE = 10
+
+for i in range(0, 500, 25):
+    g.dessinerLigne(i, 0, i, 500, 'white')
+    g.dessinerLigne(0, i, 500, i, 'white')
+
+
+def pos_valide(x, y) -> bool:
+    return 0 <= x < 20 and 0 <= y < 20 and not cadre[x][y]
+
+
+def naissance_proie(num):
+    for _ in range(num):
+        x = y = None
+        while not x or not y or cadre[x][y]:
+            x = random.randint(0, 19)
+            y = random.randint(0, 19)
+        p = Proie(APROIE, g.dessinerDisque(x * 25 + 12.5, y * 25 + 12.5, 10, 'green'), x, y)
+        cadre[x][y] = p
+        proies.append(p)
+
+
+def deplace_proie():
+    dead_proie = set()
+    for p in proies:
+        pos = [(0, 0), (-1, 0), (0, -1), (1, 0), (0, 1), (1, 1), (-1, -1), (1, -1), (-1, 1)]
+        x, y = random.choice(pos)
+        while len(pos) > 0 and not pos_valide(x + p.cadre_x, y + p.cadre_y):
+            x, y = random.choice(pos)
+            pos.remove((x, y))
+        if len(pos) > 1:  # si deplacement
+            cadre[p.cadre_x][p.cadre_y] = None
+            g.deplacer(p.graph, x * 25, y * 25)
+            p.cadre_y += y
+            p.cadre_x += x
+            cadre[p.cadre_x][p.cadre_y] = p
+        p.age -= 1
+        if p.age == 0:
+            dead_proie.add(p)
+        g.actualiser()
+
+    for p in dead_proie:
+        proies.remove(p)
+        cadre[p.cadre_x][p.cadre_y] = None
+        g.supprimer(p.graph)
+
+
+def place_libre(p1, p2):
+    positions = [(0, 0), (-1, 0), (0, -1), (1, 0), (0, 1), (1, 1), (-1, -1), (1, -1), (-1, 1)]
+    for pos in positions:
+        if pos_valide(p1.cadre_x + pos[0], p1.cadre_y + pos[1]):
+            return p1.cadre_x + pos[0], p1.cadre_y + pos[1]
+        if pos_valide(p2.cadre_x + pos[0], p2.cadre_y + pos[1]):
+            return p2.cadre_x + pos[0], p2.cadre_y + pos[1]
+
+
+def reprod_proie():
+    en_couple = set()
+    new_proies = []
+    for p1 in proies:
+        for p2 in proies:
+            if p1 != p2 and proie_proches(p1, p2) and p1 not in en_couple and p2 not in en_couple:
+                en_couple.update([p1, p2])
+                new_pos = place_libre(p1, p2)
+                if new_pos:
+                    p = Proie(4, g.dessinerDisque(new_pos[0] * 25 + 12.5, new_pos[1] * 25 + 12.5, 10, 'green'),
+                              *new_pos)
+                    new_proies.append(p)
+                    cadre[p.cadre_x][p.cadre_y] = p
+                    g.actualiser()
+    proies.extend(new_proies)
+
+
+def proie_proches(p1, p2):
+    return p1.graph.x == p2.graph.x or p1.graph.y == p2.graph.y \
+           or abs(p1.graph.x - p2.graph.x) == 25 or abs(p1.graph.y - p2.graph.y) == 25
+
+
+def naissance_pred(num):
+    for _ in range(num):
+        x = y = None
+        while not x or not y or cadre[x][y]:
+            x = random.randint(0, 19)
+            y = random.randint(0, 19)
+        p = Predateur(EPRE, g.dessinerDisque(x * 25 + 12.5, y * 25 + 12.5, 10, 'red'), x, y)
+        cadre[x][y] = p
+        predateurs.append(p)
+
+
+def find_proie(pred, n) -> Proie | None:
+    if len(proies) == 0:
+        return
+    positions = list(itertools.product(range(-n, n + 1), range(-n, n + 1)))
+    start = True
+    while start or len(positions) > 1 and not isinstance(cadre[nx][ny], Proie):
+
+        x, y = random.choice(positions)
+        nx = x + pred.cadre_x
+        ny = y + pred.cadre_y
+        nx = 0 if nx < 0 else 19 if nx > 19 else nx
+        ny = 0 if ny < 0 else 19 if ny > 19 else ny
+        positions.remove((x, y))
+        start = False
+    return cadre[nx][ny] if len(positions) > 1 else None
+
+
+def deplace_pred():
+    if len(proies) == 0:
+        return
+    for pred in predateurs:
+        n = 1
+        proie = find_proie(pred, n)
+        while not proie:
+            n += 1
+            proie = find_proie(pred, n)
+            if n > 5:
+                return
+
+        cadre[pred.cadre_x][pred.cadre_y] = None
+        dx = 1 if proie.cadre_x > pred.cadre_x else -1 if proie.cadre_x < pred.cadre_x else 0
+        dy = 1 if proie.cadre_y > pred.cadre_y else -1 if proie.cadre_y < pred.cadre_y else 0
+        if not cadre[pred.cadre_x][pred.cadre_y]:
+            pred.cadre_x += dx
+            pred.cadre_y += dy
+            cadre[pred.cadre_x][pred.cadre_y] = pred
+            g.deplacer(pred.graph, dx * 25, dy * 25)
+
+        if (pred.cadre_x, pred.cadre_y) == (proie.cadre_x, proie.cadre_y):
+            g.supprimer(proie.graph)
+            proies.remove(proie)
+            pred.energie += 2
+            if len(proies) == 0:
+                return
+
+        if pred.energie % 8 == 0:
+            naissance_pred(1)
+        g.actualiser()
+
+
+def death_pred():
+    i = 0
+    for pred in predateurs:
+        pred.energie -= 1
+        if pred.energie == 0:
+            i += 1
+            g.supprimer(pred.graph)
+            predateurs.remove(pred)
+            cadre[pred.cadre_x][pred.cadre_y] = None
+
+
+liste_pred = []
+liste_proie = []
+
+naissance_proie(10)
+naissance_pred(3)
+for _ in range(500):
+    liste_proie.append(len(proies))
+    liste_pred.append(len(predateurs))
+    naissance_proie(5)
+    deplace_proie()
+    g.actualiser()
+    reprod_proie()
+    g.actualiser()
+    deplace_pred()
+    g.actualiser()
+    death_pred()
+    if len(predateurs) == 0 or len(proies) == 0:
+        break
+
+import matplotlib.pyplot as plt
+
+X = range(len(liste_proie))
+plt.plot(X, liste_pred,'r',label="Prédateurs")
+plt.plot(X, liste_proie,'g', label="Proies")
+
+plt.xlabel("Nombre de tours")
+plt.ylabel("Taille de la population")
+plt.legend(loc="upper left")
+
+plt.show()
+
+g.attendreClic()
+g.fermerFenetre()
