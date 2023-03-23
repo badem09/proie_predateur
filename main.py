e196f6432141c852b11f466fef8b74cb7a8d6065
Demba BA <badembajob@gmail.com>
2023-03-23 23:42:56 +0100

    Ajout commentaire / doc Généralisation des variable globales

--- main.py
+++ main.py
@@ -3,36 +3,28 @@ import tkiteasy as tki
 from characters import Proie, Predateur
 import random
 
-g = tki.ouvrirFenetre(500, 500)
-proies = []
-predateurs = []
-liste = [None].copy() * 20
-cadre = [liste.copy() for _ in range(20)]
-EPRE = 4
-APROIE = 10
-
-for i in range(0, 500, 25):
-    g.dessinerLigne(i, 0, i, 500, 'white')
-    g.dessinerLigne(0, i, 500, i, 'white')
-
 
 def pos_valide(x, y) -> bool:
-    return 0 <= x < 20 and 0 <= y < 20 and not cadre[x][y]
+    """
+    :param x:
+    :param y:
+    :return: bool en fonction de la validité disponibilité de la postition
+    """
+    return 0 <= x < NB_CASES and 0 <= y < NB_CASES and not cadre[x][y]
 
 
 def naissance_proie(num):
     for _ in range(num):
         x = y = None
-        while not x or not y or cadre[x][y]:
-            x = random.randint(0, 19)
-            y = random.randint(0, 19)
-        p = Proie(APROIE, g.dessinerDisque(x * 25 + 12.5, y * 25 + 12.5, 10, 'green'), x, y)
+        while not x or not y or cadre[x][y]:  # recherche d'une case libre.
+            x = random.randint(0, NB_CASES - 1)
+            y = random.randint(0, NB_CASES - 1)
+        p = Proie(APROIE, g.dessinerDisque(x * RATIO + RATIO/2, y * RATIO + RATIO/2, RATIO/3, 'green'), x, y)
         cadre[x][y] = p
         proies.append(p)
 
 
 def deplace_proie():
-    dead_proie = set()
     for p in proies:
         pos = [(0, 0), (-1, 0), (0, -1), (1, 0), (0, 1), (1, 1), (-1, -1), (1, -1), (-1, 1)]
         x, y = random.choice(pos)
@@ -41,23 +33,28 @@ def deplace_proie():
             pos.remove((x, y))
         if len(pos) > 1:  # si deplacement
             cadre[p.cadre_x][p.cadre_y] = None
-            g.deplacer(p.graph, x * 25, y * 25)
+            g.deplacer(p.graph, x * RATIO, y * RATIO)
             p.cadre_y += y
             p.cadre_x += x
             cadre[p.cadre_x][p.cadre_y] = p
+    death_proies()
+
+def death_proies():
+    dead_proie = set()
+    for p in proies:
         p.age -= 1
         if p.age == 0:
             dead_proie.add(p)
         g.actualiser()
 
-    for p in dead_proie:
+    for p in dead_proie:  #la faucheuse
         proies.remove(p)
         cadre[p.cadre_x][p.cadre_y] = None
         g.supprimer(p.graph)
 
 
 def place_libre(p1, p2):
-    positions = [(0, 0), (-1, 0), (0, -1), (1, 0), (0, 1), (1, 1), (-1, -1), (1, -1), (-1, 1)]
+    positions = [(-1, 0), (0, -1), (1, 0), (0, 1), (1, 1), (-1, -1), (1, -1), (-1, 1)]
     for pos in positions:
         if pos_valide(p1.cadre_x + pos[0], p1.cadre_y + pos[1]):
             return p1.cadre_x + pos[0], p1.cadre_y + pos[1]
@@ -74,7 +71,7 @@ def reprod_proie():
                 en_couple.update([p1, p2])
                 new_pos = place_libre(p1, p2)
                 if new_pos:
-                    p = Proie(4, g.dessinerDisque(new_pos[0] * 25 + 12.5, new_pos[1] * 25 + 12.5, 10, 'green'),
+                    p = Proie(4, g.dessinerDisque(new_pos[0] * RATIO + RATIO/2, new_pos[1] * RATIO + RATIO/2, RATIO/3, 'green'),
                               *new_pos)
                     new_proies.append(p)
                     cadre[p.cadre_x][p.cadre_y] = p
@@ -82,34 +79,46 @@ def reprod_proie():
     proies.extend(new_proies)
 
 
-def proie_proches(p1, p2):
+def proie_proches(p1, p2) -> bool:
+    """
+    :param p1: Proie
+    :param p2: Proie
+    :return: retourne un booléan en fonction de la proximité ou non de deux proies
+    """
     return p1.graph.x == p2.graph.x or p1.graph.y == p2.graph.y \
-           or abs(p1.graph.x - p2.graph.x) == 25 or abs(p1.graph.y - p2.graph.y) == 25
+           or abs(p1.graph.x - p2.graph.x) == RATIO or abs(p1.graph.y - p2.graph.y) == RATIO
 
 
 def naissance_pred(num):
     for _ in range(num):
         x = y = None
         while not x or not y or cadre[x][y]:
-            x = random.randint(0, 19)
-            y = random.randint(0, 19)
-        p = Predateur(EPRE, g.dessinerDisque(x * 25 + 12.5, y * 25 + 12.5, 10, 'red'), x, y)
+            x = random.randint(0, NB_CASES -1)
+            y = random.randint(0, NB_CASES -1)
+        p = Predateur(EPRE, g.dessinerDisque(x * RATIO + RATIO/2, y * RATIO + RATIO/2, RATIO/3, 'red'), x, y)
         cadre[x][y] = p
         predateurs.append(p)
 
 
 def find_proie(pred, n) -> Proie | None:
+    """
+    Trouve la proie l'une des proies les plus proches à n cases.
+    :param pred: Predateur
+    :param n: distance en cases
+    :return: Proie | None
+    """
     if len(proies) == 0:
         return
     positions = list(itertools.product(range(-n, n + 1), range(-n, n + 1)))
+    #coordonnées des cases situés à n cases
     start = True
     while start or len(positions) > 1 and not isinstance(cadre[nx][ny], Proie):
 
         x, y = random.choice(positions)
         nx = x + pred.cadre_x
         ny = y + pred.cadre_y
-        nx = 0 if nx < 0 else 19 if nx > 19 else nx
-        ny = 0 if ny < 0 else 19 if ny > 19 else ny
+        nx = 0 if nx < 0 else NB_CASES-1 if nx > NB_CASES-1 else nx
+        ny = 0 if ny < 0 else NB_CASES-1 if ny > NB_CASES-1 else ny
         positions.remove((x, y))
         start = False
     return cadre[nx][ny] if len(positions) > 1 else None
@@ -119,76 +128,94 @@ def deplace_pred():
     if len(proies) == 0:
         return
     for pred in predateurs:
-        n = 1
+        n = 1 # portée du flair
         proie = find_proie(pred, n)
         while not proie:
             n += 1
             proie = find_proie(pred, n)
-            if n > 5:
+            if n > FLAIR:
                 return
 
         cadre[pred.cadre_x][pred.cadre_y] = None
-        dx = 1 if proie.cadre_x > pred.cadre_x else -1 if proie.cadre_x < pred.cadre_x else 0
-        dy = 1 if proie.cadre_y > pred.cadre_y else -1 if proie.cadre_y < pred.cadre_y else 0
-        if not cadre[pred.cadre_x][pred.cadre_y]:
+        dx = 1 if proie.cadre_x > pred.cadre_x else -1 if proie.cadre_x < pred.cadre_x else 0  # distance x
+        dy = 1 if proie.cadre_y > pred.cadre_y else -1 if proie.cadre_y < pred.cadre_y else 0  # distance y
+        if not cadre[pred.cadre_x][pred.cadre_y]:  # case libre
             pred.cadre_x += dx
             pred.cadre_y += dy
             cadre[pred.cadre_x][pred.cadre_y] = pred
-            g.deplacer(pred.graph, dx * 25, dy * 25)
+            g.deplacer(pred.graph, dx * RATIO, dy * RATIO)
 
-        if (pred.cadre_x, pred.cadre_y) == (proie.cadre_x, proie.cadre_y):
+        if (pred.cadre_x, pred.cadre_y) == (proie.cadre_x, proie.cadre_y):  # Consommation d'une proie
             g.supprimer(proie.graph)
             proies.remove(proie)
-            pred.energie += 2
+            pred.energie += CALORIES_PROIE
             if len(proies) == 0:
                 return
 
-        if pred.energie % 8 == 0:
+        if pred.energie % F_REPR_PRED == 0:
             naissance_pred(1)
         g.actualiser()
+    death_pred()
 
 
 def death_pred():
-    i = 0
     for pred in predateurs:
         pred.energie -= 1
         if pred.energie == 0:
-            i += 1
             g.supprimer(pred.graph)
             predateurs.remove(pred)
             cadre[pred.cadre_x][pred.cadre_y] = None
 
+if __name__ == '__main__':
+    longueur = largeur = 600
+    NB_CASES = 20
+    RATIO = longueur // NB_CASES
 
-liste_pred = []
-liste_proie = []
+    g = tki.ouvrirFenetre(longueur, largeur)
+    proies = []
+    predateurs = []
+    liste = [None].copy() * NB_CASES
+    cadre = [liste.copy() for _ in range(NB_CASES)]
 
-naissance_proie(10)
-naissance_pred(3)
-for _ in range(500):
-    liste_proie.append(len(proies))
-    liste_pred.append(len(predateurs))
-    naissance_proie(5)
-    deplace_proie()
-    g.actualiser()
-    reprod_proie()
-    g.actualiser()
-    deplace_pred()
-    g.actualiser()
-    death_pred()
-    if len(predateurs) == 0 or len(proies) == 0:
-        break
+    EPRE = 4  # énergie des prédateurs
+    APROIE = 10  # durée de vie des proies
+    FLAIR = 5  # flair des prédateurs
+    F_REPR_PRED = 8  # Fréquende de reproduction des prédateurs
+    CALORIES_PROIE = 2  # Points d'énergies aquis à la consommation d'une proie
 
-import matplotlib.pyplot as plt
+    for i in range(0, longueur, longueur//NB_CASES):
+        g.dessinerLigne(i, 0, i, longueur, 'white')
+        g.dessinerLigne(0, i, longueur, i, 'white')
 
-X = range(len(liste_proie))
-plt.plot(X, liste_pred,'r',label="Prédateurs")
-plt.plot(X, liste_proie,'g', label="Proies")
+    liste_pred = []
+    liste_proie = []
 
-plt.xlabel("Nombre de tours")
-plt.ylabel("Taille de la population")
-plt.legend(loc="upper left")
+    naissance_proie(10)
+    naissance_pred(3)
+    for _ in range(500):
+        liste_proie.append(len(proies))
+        liste_pred.append(len(predateurs))
+        naissance_proie(5)
+        deplace_proie()
+        g.actualiser()
+        reprod_proie()
+        g.actualiser()
+        deplace_pred()
+        g.actualiser()
+        if len(predateurs) == 0 or len(proies) == 0:
+            break
 
-plt.show()
+    import matplotlib.pyplot as plt
 
-g.attendreClic()
-g.fermerFenetre()
+    X = range(len(liste_proie))
+    plt.plot(X, liste_pred,'r',label="Prédateurs")
+    plt.plot(X, liste_proie,'g', label="Proies")
+
+    plt.xlabel("Nombre de cycles")
+    plt.ylabel("Taille de la population")
+    plt.legend(loc="upper left")
+
+    plt.show()
+
+    g.attendreClic()
+    g.fermerFenetre()
